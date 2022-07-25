a1c43109ff725f97c4d15fa3c695c31dd8c2c3cb
Mikołaj Badyl <mbadyl@mggpaero.tar>
2022-07-25 15:07:30 +0200

    better path

--- splitter.py
+++ splitter.py
@@ -2,15 +2,14 @@ import os
 from PIL import Image
 from itertools import product
 import argparse
+Image.MAX_IMAGE_PIXELS = 933120000
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', type=str, required=True)
-parser.add_argument('--dir_in', type=str, required=True)
+parser.add_argument('--path', type=str, required=True)
 parser.add_argument('--t_size', type=int, required=True)
 args = parser.parse_args()
 
 def split(filename, dir_in, t_size):
-
     name, ext = os.path.splitext(filename)
     img = Image.open(os.path.join(dir_in, filename))
     width, height =  img.size
@@ -25,7 +24,8 @@ def split(filename, dir_in, t_size):
 
     out_folder_name= (f'{name}_tiled')
     out_folder_path = dir_in + '\\' + out_folder_name
-    os.makedirs(out_folder_name, exist_ok=True)
+    
+    os.mkdir(out_folder_path)
     
     for i, j in grid:
         box = (j, i, j + t_size, i + t_size)
@@ -34,4 +34,11 @@ def split(filename, dir_in, t_size):
 
     print('\nWygenerowano tile.')
 
-split(args.name, args.dir_in, args.t_size)
+if os.path.isdir(args.path):
+    files_list = os.listdir(args.path)
+    for img in files_list:
+        if img.endswith('.tif') or img.endswith('.jpg') or img.endswith('jpeg') or img.endswith('.png'):
+            split(img, args.path, args.t_size)
+else:
+    split(os.path.basename(args.path), args.path + '\\..', args.t_size)
+
